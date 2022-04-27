5045a978931a0a11505244813761386c0a46db33
Louis Bacon <louis.bacon17@icloud.com>
2022-04-27 20:17:39 +0100

    created wallet component

---
+++ front_end/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { DAppProvider, ChainId } from '@usedapp/core';
+import {Header} from './components/Header'
+import {Container} from '@material-ui/core'
+import {Main} from './components/Main'
+
+function App() {
+  return (
+    <DAppProvider config={{
+      supportedChains: [ChainId.Kovan]
+    }}>
+      <Header />
+      <Container maxWidth='md'>
+        <div>Hi!</div>
+        <Main />
+      </Container>ß
+    </DAppProvider>
+  );
+}
+
+export default App;
--- scripts/deploy.py
+++ scripts/deploy.py
@@ -2,12 +2,16 @@ from webbrowser import get
 from scripts.helpful_scripts import get_account, get_contract
 from brownie import DappToken, TokenFarm, config, network
 from web3 import Web3
+import yaml
+import json
+import os
+import shutil
 
 
 KEPT_BALANCE = Web3.toWei(100, "ether")
 
 
-def deploy_token_farm_and_dapp_token():
+def deploy_token_farm_and_dapp_token(front_end_update=False):
     account = get_account()
     dapp_token = DappToken.deploy({"from": account})
     token_farm = TokenFarm.deploy(
@@ -28,6 +32,8 @@ def deploy_token_farm_and_dapp_token():
         weth_token: get_contract("eth_usd_price_feed"),
     }
     add_allowed_tokens(token_farm, dict_of_allowed_tokens, account)
+    if update_front_end:
+        update_front_end()
     return token_farm, dapp_token
 
 
@@ -42,5 +48,22 @@ def add_allowed_tokens(token_farm, dict_of_allowed_tokens, account):
     return token_farm
 
 
+def update_front_end():
+    # Send the build folder
+    copy_folders_to_front_end("./build", "./front_end/src/chain-info")
+    # Send our config to front end in json format
+    with open("brownie-config.yaml", "r") as config:
+        config_dict = yaml.load(config, Loader=yaml.FullLoader)
+        with open("./front_end/src/brownie-config.json", "w") as config_json:
+            json.dump(config_dict, config_json)
+    print("Front end updated!")
+
+
+def copy_folders_to_front_end(src, dst):
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+    shutil.copytree(src, dst)
+
+
 def main():
-    deploy_token_farm_and_dapp_token()
+    deploy_token_farm_and_dapp_token(front_end_update=True)
